832951062f79075fd1206ff5ea6415ce2205a2ef
Luis Milberg <luismilberg@gmail.com>
2021-06-04 14:40:55 -0300

    correción de dependencias

--- index.js
+++ index.js
@@ -8,11 +8,6 @@ const exphbs = require('express-handlebars')
 const router = require('./routes/index')
 const path = require('path');
 const bodyParser = require('body-parser');
-const passport = require('./config/passport');
-const session = require('express-session');
-const cookieParser = require('cookie-parser');
-const MongoStore = require('connect-mongo');
-const flash = require('connect-flash');
 
 const app = express();
 
@@ -20,7 +15,6 @@ const app = express();
 app.engine('handlebars', 
     exphbs({
         defaultLayout: 'layout',
-        // helpers: require('./helpers/handlebars'),
         allowProtoMethodsByDefault: true
     })
 );
@@ -31,25 +25,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // View Engine
 app.set('view engine', 'handlebars');
 
-
 // Static Files
 app.use(express.static(path.join(__dirname,'public')));
-
-// Cookieparser y sesiones
-app.use(cookieParser());
-app.use(session({
-    resave: false,
-    saveUninitialized: false,
-    secret: 'foo',
-    store: MongoStore.create({mongoUrl: process.env.DB})
-  }));
-
-// Inicializar passport
-app.use(passport.initialize());
-app.use(passport.session());
-
-// Flash
-app.use(flash());
 
 // Router
 app.use('/', router());
